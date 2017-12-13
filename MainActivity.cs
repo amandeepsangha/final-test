c5275cb8f8ecfa4bcf1224196b1f6d8e199b467d
amandeepsangha <32910289+amandeepsangha@users.noreply.github.com>
2017-12-13 16:15:38 -0500

    Add files via upload

---
+++ MainActivity.cs
@@ -0,0 +1,47 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using App2;
+
+namespace app2
+{
+    [Activity(Label = "app2", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            SetContentView(Resource.Layout.Main);
+
+            Button btnInsert = FindViewById<Button>(Resource.Id.btnInsertProduct);
+
+            btnInsert.Click += delegate
+            {
+                StartActivity(typeof(insertproduct));
+            };
+
+            Button btnDisplay = FindViewById<Button>(Resource.Id.btnDisplayProduct);
+            btnDisplay.Click += delegate
+            {
+                StartActivity(typeof(DisplayProduct));
+            };
+
+            Button btnList = FindViewById<Button>(Resource.Id.btnViewInventory);
+            btnList.Click += delegate
+            {
+                StartActivity(typeof(ViewInventory));
+            };
+
+        }
+
+        private class ViewInventory
+        {
+        }
+    }
+
+    internal class DisplayProduct
+    {
+    }
+}
+
---
+++ product.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace App2
+{
+    class product
+    {
+        [PrimaryKey, AutoIncrement, Column("_Id")]
+
+        public int ProductId { get; set; } // AutoIncrement and set primarykey  
+
+        [MaxLength(40)]
+
+        public string Name { get; set; }
+
+        [MaxLength(40)]
+
+        public string Price { get; set; }
+
+
+
+        public string Quantity { get; set; }
+    }
+
+    }
+}
+
+}
